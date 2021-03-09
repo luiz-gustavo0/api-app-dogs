c5d2ce5669deebf9661b7d501759c0128cb34361
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-03-09 17:15:32 -0300

    create migration users

---
+++ src/database/migrations/20210309200817-create-users.js
@@ -0,0 +1,39 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password_hash: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users')
+  },
+}
