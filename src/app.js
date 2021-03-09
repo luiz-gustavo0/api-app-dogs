eb3160547bcafe4b9b1fc1680f96df0706db4712
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-03-09 17:04:12 -0300

    commit inicial

---
+++ src/app.js
@@ -0,0 +1,11 @@
+require('dotenv/config')
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+module.exports = app
